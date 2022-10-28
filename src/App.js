e2d7aafaaf75ae791fc54df941b29d9cc0431007
Lyudmila <milo4kamoya@gmail.com>
2022-10-28 11:37:31 -0700

    Sounds added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Keyboard from './components/Keyboard';
+import soundsOne from './utils/soundsOne';
+import soundsTwo from './utils/soundsTwo';
 
 function App() {
+  console.log('soundsOne', soundsOne);
+  console.log('soundsTwo', soundsTwo);
+  
   return (
     <div id="drum-machine">
-      
+      <Keyboard />
     </div>
   );
 }
