3996a8ded442fae62d91ea5f43b0077aa5f6ff78
Lyudmila <milo4kamoya@gmail.com>
2022-10-28 11:46:35 -0700

    Display all buttons

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,5 +1,15 @@
+import soundsOne from "../utils/soundsOne"
+
 export default function Keyboard() {
-  return (
-    <div>Keyboard</div>
-  )
+  return soundsOne.map(sound => {
+    return (
+      <button className="drum-pad">
+        <audio
+          src={sound.url}
+          id={sound.key}
+          className="clip" />
+        {sound.key}
+      </button>
+    )
+  })
 }
