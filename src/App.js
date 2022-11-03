f8b5733b31046419c889b0bde1e38a41c8c1520a
Lyudmila <milo4kamoya@gmail.com>
2022-11-03 10:48:45 -0700

    Turn On and Off the Power

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ChangeSounds from './components/ChangeSounds';
 import Display from "./components/Display";
 import GetSound from './components/GetSound';
+import Power from "./components/Power";
 
 import soundsOne from "./utils/soundsOne";
 import soundsTwo from "./utils/soundsTwo"
@@ -17,20 +18,26 @@ function App() {
   const [soundType, setSoundType] = useState("heaterKit");
   const [sounds, setSounds] = useState(soundsGroup[soundType]);
   const [displaySoundName, setDisplaySoundName] = useState(soundType);
+  const [power, setPower] = useState(true)
 
   function playSound(el) {
-    const audio = document.getElementById(el);
-    audio.currentTime = 0;
-    audio.play();
-    const nameToDisplay = soundsGroup[soundType].filter(e => {
-      if (e.key === audio.id) {
-        return e.id
-      }
-    });
-    setDisplaySoundName(nameToDisplay[0].id);
+    if(power) {
+      const audio = document.getElementById(el);
+      audio.currentTime = 0;
+      audio.play();
+      const nameToDisplay = soundsGroup[soundType].filter(e => {
+        if (e.key === audio.id) {
+          return e.id
+        }
+      });
+      setDisplaySoundName(nameToDisplay[0].id);
+    } if (!power) {
+      setDisplaySoundName("Turn On The Power")
+
+    }
   }
 
-  function changeSound(s) {
+  function changeSound() {
     if (soundType === "heaterKit") {
       setSoundType("smoothPianoKit")
       setSounds(soundsGroup.smoothPianoKit)
@@ -42,12 +49,17 @@ function App() {
     }
   }
 
+  function changePower() {
+    setPower(!power);
+  }
+
   return (
     <div id="drum-machine" className="container">
       <div className="drum-pad-container">
         <GetSound playSound={playSound} sounds={sounds} />
       </div>
       <div className="control-container">
+        <Power power={changePower}/>
         <Display displayName={displaySoundName} />
         <ChangeSounds changeSound={changeSound} />
       </div>
--- src/components/Power.js
+++ src/components/Power.js
@@ -1,19 +1,19 @@
 import { useState } from "react"
 import './changeSounds.css';
 
-export default function ChangeSounds(props) {
+export default function Power(props) {
   const [isActive, setIsActive] = useState(false);
   const activeCls = isActive ? 'is-active' : '';
   const activeTxt = isActive ? 'On' : 'Off';
 
   function toggle() {
     setIsActive(!isActive);
-    props.changeSound();
+    props.power()
   }
 
   return (
     <div className="bank">
-      <label>Bank</label>
+      <label>Power {!isActive ? 'is On' : 'is Off'}</label>
       <button className={'SlideBtn ' + activeCls} onClick={toggle}>
         <span className="SlideBtn-label visuallyhidden">{activeTxt}</span>
         <span className="SlideBtn-knob"></span>
